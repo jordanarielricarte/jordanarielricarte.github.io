79c749da28fb9abb70d259267a04c7872ae5c66c
Sergio Ramirez <ser.ramirez@alumnos.duoc.cl>
2018-11-27 23:11:27 -0300

    Agrega js nuevo y modifica algunos html

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = "Mis Perris V3";
+var cacheName = "MisPerris-V3";
 var filesToCache = [
     "/",
     "/index.html",
@@ -9,6 +9,7 @@ var filesToCache = [
     "/JavaScript/app.js",
     "/JavaScript/ciudad.js",
     "/JavaScript/main.js",
+    "/JavaScript/api.js",
     "/style/estilos.css",
     "/style/final.css",
     "/style/fontello.css",
@@ -34,39 +35,40 @@ var filesToCache = [
     "/imagenes/logo.jpg",
     "/imagenes/poseidon.jpg",
     "/imagenes/thor.jpg",
-    "/imagenes/zeus.jpg",
+    "/imagenes/zeus.jpg"
 ];
 
-self.addEventListener( 'install', function( e ) {
-  console.log( '[ServiceWorker] Install' );
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
   e.waitUntil(
-      caches.open( cacheName ).then( function( cache ) {
-          console.log( '[ServiceWorker] Caching app shell' );
-          return cache.addAll( filesToCache );
-      } )
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
   );
-} );
+});
 
-self.addEventListener( 'activate', function( e ) {
-  console.log( '[ServiceWorker] Activate' );
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
   e.waitUntil(
-      caches.keys( ).then( function( keyList ) {
-          return Promise.all( keyList.map( function( key ) {
-              if ( key != cacheName ) {
-                  console.log('[ServiceWorker] Removing old cache', key);
-                  return caches.delete( key );
-              }
-          }));
-      })
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
   );
+ 
   return self.clients.claim();
 });
 
-self.addEventListener( 'fetch', function( e ) {
-  console.log( '[ServiceWorker] Fetch', e.request.url );
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
-      caches.match( e.request ).then( function( response ) {
-          return response || fetch( e.request );
-      } )
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
-} );
+});
